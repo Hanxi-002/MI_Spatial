93b81049ea44a5ef23070ce7b8159e6d314f8ff5
Hanxi Xiao <xiaoh@pitt.edu>
2023-11-20 16:56:29 -0500

    remake x and y for concat

--- RFAF_WithinRegions.R
+++ RFAF_WithinRegions.R
@@ -75,7 +75,7 @@ dim(x)
 dim(y)
 
 
-#write.csv(x, '/Users/xiaoh/Library/CloudStorage/OneDrive-UniversityofPittsburgh/MI_Spatial/ER_SLIDE/Within_Region/AR_RF_Concat/111523/Data/x_norm.csv')
+#write.csv(x, '/Users/xiaoh/Library/CloudStorage/OneDrive-UniversityofPittsburgh/MI_Spatial/ER_SLIDE/Within_Region/AF_RF_Concat/111523/Data/x.csv')
 #write.csv(y, '/Users/xiaoh/Library/CloudStorage/OneDrive-UniversityofPittsburgh/MI_Spatial/ER_SLIDE/Within_Region/AF_RF_Concat/111523/Data/y.csv', row.names = FALSE)
 
 # colnames(norm_mat) <- trim_strings(colnames(norm_mat))
