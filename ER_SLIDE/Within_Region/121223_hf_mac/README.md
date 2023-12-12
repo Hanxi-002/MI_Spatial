f1288c851615cd0f204db5735cdf3d4594afcb7d
Hanxi Xiao <xiaoh@pitt.edu>
2023-12-12 10:25:26 -0500

    get data for CD68 Y=dist

---
+++ ER_SLIDE/Within_Region/121223_hf_mac/README.md
@@ -0,0 +1,14 @@
+# Within\_Region\_ER\_121223
+
+## Background / Motivation
+Assign regions to 2 categories "close to resting fibroblast" and "closet to active fibroblast" by looking at the porportion of resting vs fibro in each region.<br>
+This analysis only includes the HF regions and CD68 AOI. <br>
+
+This is a repeated analysis intend to get the clean results and clan box plots to put into the manuscripts. 
+## Links
+[]()
+
+
+
+## Notes
+* 
--- within_region_label.py
+++ within_region_label.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import numpy as np
-import rpy2
 
-annot = pd.read_excel("/Users/xiaoh/Library/CloudStorage/OneDrive-UniversityofPittsburgh/MI_Spatial/raw_data/Dcc_Initial_Dataset_Dutta02.xlsx")
+
+annot = pd.read_excel("/Users/xiaoh/Library/CloudStorage/OneDrive-UniversityofPittsburgh/MI_Spatial/raw_data/Final_Annotation_Dutta_sample_info.xlsx")
 
 sum_count = annot.groupby('SegmentLabel').sum()
 
-y = pd.DataFrame(annot["DccNames"])
+y = pd.DataFrame(annot["DCCnames"])
 y['Labels'] = np.full([len(y)], np.nan)
 #%%
 for s in annot["SlideName"].unique():
@@ -16,14 +16,58 @@ for s in annot["SlideName"].unique():
         if len(df_roi) == 3:
             resting_ratio = df_roi[df_roi["SegmentLabel"] == "resting fibroblast"]["AOINucleiCount"].values[0] / sum_count.loc["resting fibroblast"]['AOINucleiCount']
             active_ratio = df_roi[df_roi["SegmentLabel"] == "active fibroblast"]["AOINucleiCount"].values[0] / sum_count.loc["active fibroblast"]['AOINucleiCount']
-            dcc = df_roi[df_roi["SegmentLabel"] == "CD 68"]["DccNames"].values[0]
+            dcc = df_roi[df_roi["SegmentLabel"] == "CD 68"]["DCCnames"].values[0]
             print(resting_ratio)
             print(active_ratio)
             
             if resting_ratio > active_ratio:
-                y.loc[y["DccNames"] == dcc, "Labels"] = 0
+                y.loc[y["DCCnames"] == dcc, "Labels"] = 0
             elif resting_ratio < active_ratio:    
-                y.loc[y["DccNames"] == dcc, "Labels"] = 1
+                y.loc[y["DCCnames"] == dcc, "Labels"] = 1
 
 y = y.dropna()
-y.to_csv("/Users/xiaoh/Library/CloudStorage/OneDrive-UniversityofPittsburgh/MI_Spatial/ER_SLIDE/Within_Region/052223/Data/y.csv", index = False)
+#y.to_csv("/Users/xiaoh/Library/CloudStorage/OneDrive-UniversityofPittsburgh/MI_Spatial/ER_SLIDE/Within_Region/052223/Data/y.csv", index = False)
+y.to_csv("/Users/xiaoh/Library/CloudStorage/OneDrive-UniversityofPittsburgh/MI_Spatial/ER_SLIDE/Within_Region/y.csv", index = False)
+#%% Get the annotation file including the distance labels
+# produce Y for RF and AF concatenated analyses
+annot = pd.read_excel("/Users/xiaoh/Library/CloudStorage/OneDrive-UniversityofPittsburgh/MI_Spatial/raw_data/Final_Annotation_Dutta_sample_info.xlsx")
+sum_count = annot.groupby('SegmentLabel').sum()
+
+# delete the rows where Dccnames is  "DSP-1001660011816-D-B11"
+annot = annot[annot["DCCnames"] != "DSP-1001660011816-D-B11"]
+# delete the rows where Dccnames is  "DSP-1001660012268-C-H09"
+annot = annot[annot["DCCnames"] != "DSP-1001660012268-C-H09"]
+
+
+annot_hf = annot[annot["Status"] == "HF"]
+annot_hf['AFRF_Ratio'] = np.full([len(annot_hf)], np.nan)
+# load 
+for s in annot_hf["SlideName"].unique():
+    df_slide = annot_hf[annot_hf["SlideName"] == s]
+    for r in df_slide["ROILabel"].unique():
+        df_roi = df_slide[df_slide["ROILabel"] == r]
+        if len(df_roi) == 3:
+            resting_ratio = df_roi[df_roi["SegmentLabel"] == "resting fibroblast"]["AOINucleiCount"].values[0] / sum_count.loc["resting fibroblast"]['AOINucleiCount']
+            active_ratio = df_roi[df_roi["SegmentLabel"] == "active fibroblast"]["AOINucleiCount"].values[0] / sum_count.loc["active fibroblast"]['AOINucleiCount']
+            #dcc = df_roi[df_roi["SegmentLabel"] == "CD 68"]["DCCnames"].values[0]
+            print(resting_ratio)
+            print(active_ratio)
+            
+            if resting_ratio > active_ratio:
+                label = [0] * 3
+                #y.loc[y["DCCnames"] == dcc, "Labels"] = 0
+            elif resting_ratio < active_ratio:    
+                label = [1] * 3
+                #y.loc[y["DCCnames"] == dcc, "Labels"] = 1
+            annot_hf.loc[(annot_hf["SlideName"] == s) & (annot_hf["ROILabel"] == r), "AFRF_Ratio"]= label
+
+# drop the rows where AFRF_Ratio is nan
+annot_hf = annot_hf.dropna(subset = ["AFRF_Ratio"])
+annot_hf.to_csv("/Users/xiaoh/Library/CloudStorage/OneDrive-UniversityofPittsburgh/MI_Spatial/ER_SLIDE/Within_Region/HF_Final_Annotation_RFAF_Ratio.csv", index = False)
+
+
+
+
+
+
+
