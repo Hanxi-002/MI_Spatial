4cd54252febd250e21aa4aba38819a38f6c88ea3
Hanxi Xiao <hxiao2@r314.ib.bridges2.psc.edu>
2025-04-08 10:32:22 -0400

    plotting code for svm

--- siRNA_Analyses/Pairwise_Classifier/svm.py
+++ siRNA_Analyses/Pairwise_Classifier/svm.py
@@ -1,14 +1,15 @@
 """
 Process the qPCR data and the siRNA data
 Train a svm model for each pair of siRNA targets
+ENV:MI_Spatial
 """
 import numpy as np
 import pandas as pd
 from sklearn.svm import SVC
-from sklearn.model_selection import cross_val_score, StratifiedKFold
+from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import roc_auc_score
 from sklearn.preprocessing import LabelEncoder
-from itertools import combinations
+import plotly.graph_objects as go
 
 
 #%% #############################################
@@ -73,7 +74,7 @@ def train_svm(data, target_1, target_2, n_splits=3):
 
 def main(num_iter, data, pairwise_targets):
     auc_all_targets = {}
-    for i in range(100): 
+    for i in range(num_iter): 
         print(f"The {i}th round")
         # for each target pair, perform k-fold cross validation
         for p in pairwise_targets:
@@ -134,7 +135,7 @@ auc_all_targets = auc_all_targets.reindex(index=['mean'] + list(auc_all_targets.
 
 #auc_all_targets.to_csv("/ix/djishnu/Hanxi/MI_Spatial/siRNA_Analyses/Pairwise_Classifier/freq_auc_all_targets.csv")
 #%% #############################################
-
+# train the model for the flow MFI results
 data = pd.read_excel("/ix/djishnu/Hanxi/MI_Spatial/siRNA_Analyses/Raw_Data/reformatted_flow_MFI.xlsx")
 
 
@@ -153,44 +154,119 @@ auc_all_targets = auc_all_targets.reindex(index=['mean'] + list(auc_all_targets.
 
 #auc_all_targets.to_csv("/ix/djishnu/Hanxi/MI_Spatial/siRNA_Analyses/Pairwise_Classifier/MFI_auc_all_targets.csv")
 
-
-
 #%% #############################################
-########## the code below is not what we want ###############
-
-# apply the trained model on each of the siRNA targets
-
-# def predict_target_prob(target, trained_model):
-#     """Calculate the probability of the target being a positive sample\
-#        based on the trained model.
-
-#     Args:
-#         target (_str_): target name
-#         trained_model (_model_): a trained model
-
-#     Returns:
-#         _ndarray_: a numpy array of shape: # samples by # classes\
-#                    each row sums up to be 1. 
-#     """
-#     target_data = data.loc[data['Target'].str.contains(target)]
-#     target_data = target_data.drop('Target', axis=1)
-#     prob = trained_model.predict_proba(target_data)
-#     return prob
+# plot the aucs
+auc_qPCR = pd.read_csv("siRNA_Analyses/Pairwise_Classifier/qPCR_auc_all_targets.csv", index_col=0)
+print(auc_qPCR.shape)
+auc_freq = pd.read_csv("siRNA_Analyses/Pairwise_Classifier/freq_auc_all_targets.csv", index_col=0)
+print(auc_freq.shape)
+auc_MFI = pd.read_csv("siRNA_Analyses/Pairwise_Classifier/MFI_auc_all_targets.csv", index_col=0)
+print(auc_MFI.shape)
 
 
-# # train the svm model
-# svm_model = SVC(probability=True)
-# svm_model.fit(X, y) # train on all nt and tgfb data
+# merge the mean rows of the three dataframes
+auc_qPCR = auc_qPCR.loc[auc_qPCR.index == 'mean']
+auc_freq = auc_freq.loc[auc_freq.index == 'mean']
+auc_MFI = auc_MFI.loc[auc_MFI.index == 'mean']
+# merge the three dataframes
+plot_df = pd.concat([auc_qPCR, auc_freq, auc_MFI], axis=0)
+plot_df.index = ['qPCR', 'freq', 'MFI']
+plot_df.shape
+# drop the columns that have NaN
+# plot_df = plot_df.dropna(axis=1)
+# plot_df.shape
 
-# # predict the probability of each target and save in a dictionary
-# all_target_prob = {}
-# for target in data['Target'].unique():
-#     if target == 'NT' or target == 'Tgfb':
-#         continue
-#     print(f"Target: {target}")
-#     target_prob = predict_target_prob(target, svm_model)
-#     all_target_prob[target] = target_prob
+# split the plot_df into 2 parts: TF and NonTF
+human_TFs = pd.read_csv('Cell_Oracle/allTFs_hg38_Scenic.txt', sep='\t', header=None)
+human_TF_set = set(human_TFs[0].str.upper())
+gene_names = ['Ddx5', 'B3galnt1', 'Hnrnph3', 'Npipb6', 'Tdrd6', 'Tvp23b', 'Aasdhppt', 'Tgfb', 'Maz', 'Mafk', 'Atf3', 'Egr1', 'Ep300', 'Klf3']
+TF_names = [f"si{g.upper()[0]}{g.lower()[1:]}_NT" for g in gene_names if g.upper() in human_TF_set]
+# add in the controls
+TF_names_all = TF_names + ['siCtrl oxLDL_NT', 'siCtrl nLDL_NT', 'Tgfb_NT']
+plot_df_TF = plot_df[TF_names_all]
+plot_df_nonTF = plot_df.drop(columns=TF_names)
 
-# all_target_prob["siDdx5"]
+# if plot_df_TF has value smaller than 0.5, than set it as 1-value
+plot_df_TF = plot_df_TF.applymap(lambda x: 1-x if x < 0.5 else x)
+plot_df_nonTF = plot_df_nonTF.applymap(lambda x: 1-x if x < 0.5 else x)
+plot_df_nonTF['siIqgap1_NT']  = [1.0, 0.5825, 0.9550]
+# drop the column siIqgap1_NT from plot_df_TF
+plot_df_nonTF = plot_df_nonTF.drop(columns=['siIqgap_NT'])
 
- 
+# plot the data
+
+def plot_grouped_bars(df, out_path):
+    #df = plot_df_nonTF
+    fig = go.Figure()
+
+    # Define colors for each metric
+    colors = {'qPCR': '#8f9162', 'freq': '#c77875', 'MFI': '#8cc8d0'}
+
+    # Track position for each group
+    group_positions = {}
+    current_position = 0
+    bar_height = 0.2
+    group_gap = 0.3
+
+    # Get all column names (gene names)
+    gene_names = sorted(df.columns.tolist())
+
+    # For each gene, create a group of bars
+    for i, gene in enumerate(gene_names):
+        group_positions[gene] = current_position
+        
+        # Add a bar for each metric if it's not NaN
+        for j, metric in enumerate(df.index):
+            value = df.loc[metric, gene]
+            
+            # Skip if value is NaN
+            if pd.notna(value):
+                fig.add_trace(go.Bar(
+                    y=[current_position + j*bar_height],  # Position for this bar
+                    x=[value],
+                    orientation='h',
+                    name=metric,
+                    legendgroup=metric,
+                    showlegend=True if i == 0 else False,  # Show legend only once per metric
+                    marker_color=colors[metric],
+                    width=bar_height,
+                    hovertemplate=f"{gene} - {metric}: %{{x:.2f}}<extra></extra>"
+                ))
+        
+        # Increase position for next group
+        current_position += len(df.index)*bar_height + group_gap
+
+    # Set axis properties
+    fig.update_layout(
+        title="AUCs ",
+        xaxis=dict(
+            title="AUC",
+            range=[0.5, 1.0],  
+            tickformat=".1f",
+            showline=True,
+            linewidth=1,
+            linecolor='black',
+        ),
+        yaxis=dict(
+            tickmode='array',
+            tickvals=[pos + bar_height for pos in group_positions.values()],
+            ticktext=list(group_positions.keys()),
+            showgrid=False,
+            showline=True,
+            linewidth=1,
+            linecolor='black',
+        ),
+        barmode='group',
+        height=500,
+        width=500,
+        plot_bgcolor='white',
+        legend_title="Metrics"
+    )
+
+    # Show the plot
+    fig.show()
+
+    fig.write_image(out_path, width=500, height=500, scale=2) 
+
+plot_grouped_bars(plot_df_TF, "siRNA_Analyses/Pairwise_Classifier/auc_plot_TF.pdf")
+plot_grouped_bars(plot_df_nonTF, "siRNA_Analyses/Pairwise_Classifier/auc_plot_nonTF.pdf")
