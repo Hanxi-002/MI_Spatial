2f5d663b28acdef55fcf0d1baaa84f3084dcaed1
Hanxi Xiao <hxiao2@r257.ib.bridges2.psc.edu>
2025-04-17 13:06:27 -0400

    updated allcell deseq code

--- ER_SLIDE/AllCell/022423/DESeq2.R
+++ ER_SLIDE/AllCell/022423/DESeq2.R
@@ -26,3 +26,15 @@ ggsave(plot = EnhancedVolcano(res,
        file = "toy.pdf"
 )
 
+setwd('/ocean/projects/cis240075p/hxiao2/MI_Spatial/ER_SLIDE/AllCell/022423')
+source('/ocean/projects/cis240075p/hxiao2/MI_Spatial/ER_SLIDE/Find_DEGs_Helper.R')
+
+count <- t(read.csv("x.csv", row.names = 1))
+meta <- read.csv("y_index.csv", row.names = 1)
+meta$Status <- NULL
+DEGs = calculate_DEGs(count, meta, file_name = 'DESeq2_res_20250417.RDS')
+
+
+custom_color = c("grey30", "grey30", "grey30", "#8FBC8F")
+file_name = "volcano_20250417.pdf"
+plot_volcano(DEGs = DEGs, custom_color = custom_color, file_name = file_name)
